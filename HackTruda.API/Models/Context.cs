78cae7ae5bbe3c432fbaee26a174a5dc2a1c0117
swamp1820 <swamp1820@yandex.ru>
2020-07-09 13:17:17 +0300

    initial api create

---
+++ HackTruda.API/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HackTruda.API.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+    }
+}
---
+++ HackTruda.API/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace HackTruda.API.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
